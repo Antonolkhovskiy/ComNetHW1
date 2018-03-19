53bd3c2802437b356617402716e47175e896f4de
Anton Olkhovskiy <olhovskijtoshic@yandex.ru>
2018-03-19 21:19:58 +0300

    testovoye soobwenie

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,5 +1,23 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 /**
  * Created by Anton on 19.03.2018.
  */
 public class UDPClient {
+
+    public static void main(String[] args) throws Exception{
+
+        DatagramSocket datagramSocket = new DatagramSocket(2002);
+
+        byte[] messageArray = new byte[200];
+        DatagramPacket datagramPacket = new DatagramPacket(messageArray, messageArray.length);
+
+        datagramSocket.receive(datagramPacket);
+
+        String messageInString = new String(messageArray);
+
+        System.out.print("Paket bil polu4en\n" + messageInString + "\n");
+
+    }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,5 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+
 /**
  * Created by Anton on 19.03.2018.
  */
 public class UDPServer {
+
+    public static void main(String[] args) throws Exception{
+
+        DatagramSocket datagramSocket = new DatagramSocket();
+
+        byte [] msgInBytes = "message kak ni kak".getBytes();
+
+        InetAddress inetAddress = InetAddress.getByName("localhost");
+
+        int port = 2002;
+
+        DatagramPacket datagramPacket = new DatagramPacket(msgInBytes, msgInBytes.length, inetAddress, port);
+
+        datagramSocket.send(datagramPacket);
+    }
 }
